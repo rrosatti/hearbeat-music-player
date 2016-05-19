e89557d39e596b4f392ef12a868b0c1ab7a03cd0
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-05-19 10:10:14 -0300

    fix button problems

--- app/src/androidTest/java/com/example/rodri/heartbeatmusicplayer/activity/ApplicationTest.java
+++ app/src/androidTest/java/com/example/rodri/heartbeatmusicplayer/activity/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.example.rodri.heartbeatmusicplayer;
+package com.example.rodri.heartbeatmusicplayer.activity;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
