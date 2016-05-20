9da1d97ddf5e0f34e692e12b7c3d965074c043c2
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-05-20 12:24:52 -0300

    create Song class. Change SongsManager in order to use the Song class

---
+++ app/src/main/java/com/example/rodri/heartbeatmusicplayer/song/Song.java
@@ -0,0 +1,27 @@
+package com.example.rodri.heartbeatmusicplayer.song;
+
+/**
+ * Created by rodri on 5/20/2016.
+ */
+public class Song {
+
+    private String title;
+    private String path;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+}
--- app/src/main/java/com/example/rodri/heartbeatmusicplayer/ui/activity/SongsManager.java
+++ app/src/main/java/com/example/rodri/heartbeatmusicplayer/ui/activity/SongsManager.java
@@ -1,5 +1,7 @@
 package com.example.rodri.heartbeatmusicplayer.ui.activity;
 
+import com.example.rodri.heartbeatmusicplayer.song.Song;
+
 import java.io.File;
 import java.io.FilenameFilter;
 import java.util.ArrayList;
@@ -11,7 +13,7 @@ import java.util.HashMap;
 public class SongsManager {
 
     final String MEDIA_PATH = new String("/sdcard/");
-    private ArrayList<HashMap<String, String>> songList = new ArrayList<>();
+    private ArrayList<Song> songList = new ArrayList<>();
 
     public SongsManager() {
 
@@ -23,14 +25,14 @@ public class SongsManager {
      *
      * @return
      */
-    public ArrayList<HashMap<String, String>> getPlaylist() {
+    public ArrayList<Song> getPlaylist() {
         File home = new File(MEDIA_PATH);
 
         if (home.listFiles(new FileExtensionFilter()).length > 0) {
             for (File file : home.listFiles(new FileExtensionFilter())) {
-                HashMap<String, String> song = new HashMap<>();
-                song.put("songTitle", file.getName().substring(0, (file.getName().length() - 4)));
-                song.put("songPath", file.getPath());
+                Song song = new Song();
+                song.setTitle(file.getName().substring(0, (file.getName().length() - 4)));
+                song.setPath(file.getPath());
 
                 songList.add(song);
             }
